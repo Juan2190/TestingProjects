daecd0e8b559ca4935d72d90a7a1a35259f41204
juan2190 <jcbs.219@gmail.com>
2015-11-04 11:03:52 -0500

    Blank Project

---
+++ UITableViewExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableViewExample
+//
+//  Created by MCS on 11/4/15.
+//  Copyright © 2015 MCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
